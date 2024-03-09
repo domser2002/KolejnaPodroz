54a298ed8a0f86a7f23af81b0f99c857e5635c91
domser2002 <domser2002@o2.pl>
2024-03-09 19:55:33 +0100

    user ticket methods

--- Backend/Domain/User/User.cs
+++ Backend/Domain/User/User.cs
@@ -10,7 +10,19 @@ namespace Domain.User
     public class User : Base
     {
         public User() { }
-        public Ticket? BuyTicket()
+        public Ticket? BuyTicket(int ticketID)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Ticket> ListTickets()
+        {
+            throw new NotImplementedException();
+        }
+        public Ticket? ExchangeTicket(string newName,string newSurname)
+        {
+            throw new NotImplementedException();
+        }
+        public bool DropTicket(int ticketID)
         {
             throw new NotImplementedException();
         }
