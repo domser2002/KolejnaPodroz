96135a1cf5992a882f5fd8b414c8db978ea1c483
domser2002 <domser2002@o2.pl>
2024-03-09 22:22:33 +0100

    account info class added

---
+++ Backend/Domain/User/AccountInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.User
+{
+    public class AccountInfo
+    {
+        public string FirstName = string.Empty;
+        public string LastName = string.Empty;
+        public string Email = string.Empty;
+        public DateOnly? BirthDate = null;
+    }
+}
