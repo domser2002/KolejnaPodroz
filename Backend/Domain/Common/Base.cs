da880150753eb307d3aebb921382dcd673f7e2bd
domser2002 <domser2002@o2.pl>
2024-03-09 17:30:57 +0100

    created base class

--- Backend/Domain/Common/Base.cs
+++ Backend/Domain/Common/Base.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Domain.Common;
 
-namespace Domain.Admin
+namespace Domain.Common
 {
-    public class Admin : Base
+    public class Base
     {
+        int ID;
     }
 }
