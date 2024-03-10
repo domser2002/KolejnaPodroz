0149ba9dad8ee2f70b4777759671b1468e023a61
domser2002 <domser2002@o2.pl>
2024-03-10 15:23:02 +0100

    provider service

---
+++ Backend/Domain/Services/ProviderService.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public static class ProviderService
+    {
+        public static bool AddProvider(Provider provider)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool RemoveProvider(int providerID) 
+        {
+            throw new NotImplementedException(); 
+        }
+        public static void EditProvider(int providerID)
+        { 
+            throw new NotImplementedException(); 
+        }
+    }
+}
