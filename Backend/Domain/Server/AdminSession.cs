cfca4e9be389fa983dbaa1a89f07e5238525cd0e
domser2002 <domser2002@o2.pl>
2024-03-10 12:27:31 +0100

    created session classes

---
+++ Backend/Domain/Server/AdminSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Server
+{
+    public class AdminSession
+    {
+        public string Token = string.Empty;
+        public int AdminID;
+        public DateTime LoginTime;
+    }
+}
