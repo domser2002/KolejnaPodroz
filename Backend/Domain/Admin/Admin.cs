042eb7021044dba08ba0afd7859e0001bc72e164
domser2002 <domser2002@o2.pl>
2024-03-10 11:58:38 +0100

    admin provider methods

--- Backend/Domain/Admin/Admin.cs
+++ Backend/Domain/Admin/Admin.cs
@@ -9,5 +9,22 @@ namespace Domain.Admin
 {
     public class Admin : Base
     {
+        public bool AddProvider(Provider provider)
+        {
+            throw new NotImplementedException();
+        }
+        public bool EditProvider()
+        { 
+            throw new NotImplementedException();
+        }
+        public bool RemoveProvider(int providerID) 
+        {
+            throw new NotImplementedException();
+        }
+        public List<Provider> ListProviders() 
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
