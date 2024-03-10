20e29ff750587015bead3fc5f5a8c695a432db31
domser2002 <domser2002@o2.pl>
2024-03-10 12:44:56 +0100

    server methods

--- Backend/Domain/Server/Server.cs
+++ Backend/Domain/Server/Server.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,43 @@ using System.Threading.Tasks;
 
 namespace Domain.Server
 {
-    internal class Server
+    public class Server
     {
+        public UserSession? CreateUserSession(int userID, string token)
+        {
+            throw new NotImplementedException();
+        }
+        public AdminSession? CreateAdminSession(int adminID, string token)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Connection> GetConnections(string source, string destination) 
+        { 
+            throw new NotImplementedException(); 
+        }
+        public List<Advertisment> GetAds() 
+        { 
+            throw new NotImplementedException(); 
+        }
+        public void CalculateRankings(int userID)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateDatabase()
+        {
+            throw new NotImplementedException();
+        }
+        public bool ValidateDatabase()
+        {
+            throw new NotImplementedException();
+        }
+        public void StartTechnicalBreak()
+        {
+            throw new NotImplementedException();
+        }
+        public void EndTechnicalBreak()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
