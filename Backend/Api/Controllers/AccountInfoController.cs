9f5abe450e04829d321b118ead9371d85121611b
MiloszSobczyk <m.sobczyk.official@gmail.com>
2024-04-03 00:35:56 +0200

    Create skeleton backend

---
+++ Backend/Api/Controllers/AccountInfoController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("AccountInfo")]
+    public class AccountInfoController : ControllerBase
+    {
+        [HttpGet("{userId}")]
+        public OkResult GetInfoByUserId()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Backend/Api/Controllers/ProviderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("Provider")]
+    public class ProviderController : ControllerBase
+    {
+        [HttpGet("{providerId}")]
+        public OkResult GetProviderById()
+        {
+            return Ok();
+        }
+    }
+}
