7f8b882701784a860b4fa82b62170a35ba22b844
domser2002 <domser2002@o2.pl>
2024-04-02 23:44:49 +0200

    modified Program.cs

--- Backend/Api/Program.cs
+++ Backend/Api/Program.cs
@@ -1,8 +1,14 @@
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DomainDBContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
