bda106b47fa610f8822f49986d24ca9346cd33fa
MiloszSobczyk <m.sobczyk.official@gmail.com>
2024-04-02 20:33:24 +0200

    Commit

--- Frontend/webapp/test/unit_test.dart
+++ Frontend/webapp/test/unit_test.dart
@@ -1,7 +1,7 @@
 // This is an example unit test.
 //
 // A unit test tests a single function, method, or class. To learn more about
-// writing unit tests, visit
+// writing unit tests, visitvisualvis
 // https://flutter.dev/docs/cookbook/testing/unit/introduction
 
 import 'package:flutter_test/flutter_test.dart';
