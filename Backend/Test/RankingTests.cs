bbb648d7ef7154641f7e01a7ef2bb2319fdc87c1
dawidsroczyk <101830224+dawidsroczyk@users.noreply.github.com>
2024-04-03 08:03:11 +0200

    First Test Project (#9)
    * First Test Project
    
    * consistent naming
    
    ---------
    
    Co-authored-by: Dominik Seredyn <86359641+domser2002@users.noreply.github.com>

---
+++ Backend/Test/RankingTests.cs
@@ -0,0 +1,28 @@
+﻿namespace Test
+{
+    public class RankingServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CanGetInfo_User_ReturnsTrue()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CanReturn_Update_ReturnsTrue()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CanReturn_Update_ReturnsFalse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
