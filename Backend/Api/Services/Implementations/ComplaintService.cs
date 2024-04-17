05036d9753580aefe8c65a80f8224df8172f826b
Dominik Seredyn <86359641+domser2002@users.noreply.github.com>
2024-04-17 02:02:24 +0200

    complaint service (#37)

--- Backend/Api/Services/Implementations/ComplaintService.cs
+++ Backend/Api/Services/Implementations/ComplaintService.cs
@@ -5,26 +5,39 @@ namespace Api.Services.Implementations
 {
     public class ComplaintService : Interfaces.IComplaintService
     {
-
+        DomainDBContext ddbContext;
+        public ComplaintService(DomainDBContext dbContext) 
+        {
+            ddbContext = dbContext;
+        }
         public bool MakeComplaint(Complaint complaint)
         {
-            throw new NotImplementedException();
+            ddbContext.Complaint.Add(complaint);
+            if(ddbContext.SaveChanges() != 1) return false;
+            return true;
         }
         public bool RemoveComplaint(int complaintID)
         {
-            throw new NotImplementedException();
+            Complaint complaint = ddbContext.Complaint.Find(complaintID);
+            ddbContext.Complaint.Remove(complaint);
+            if (ddbContext.SaveChanges() != 1) return false;
+            return true;
         }
         public void EditComplaint(int complaintID)
         {
-            throw new NotImplementedException();
+            Complaint complaint = ddbContext.Complaint.Find(complaintID);
+            ddbContext.Complaint.Remove(complaint);
+            // perform edition
+            ddbContext.Complaint.Add(complaint);
+            ddbContext.SaveChanges();
         }
         public Complaint GetComplaintByID(int complaintID)
         {
-            throw new NotImplementedException();
+            return ddbContext.Complaint.Find(complaintID);
         }
         public List<Complaint> GetComplaintsByUserID(int complaintUserID)
         {
-            throw new NotImplementedException();
+            return (List<Complaint>)ddbContext.Complaint.Where(c => c.UserID == complaintUserID);
         }
     }
 }
