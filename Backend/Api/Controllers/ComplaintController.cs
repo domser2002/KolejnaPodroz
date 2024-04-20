b4ac2315c05f8e85b2aabf79ef12832898df55b7
domser2002 <domser2002@o2.pl>
2024-04-20 14:15:39 +0200

    Update workflows

---
+++ Backend/Api/Controllers/ComplaintController.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using Domain.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("Complaint")]
+    public class ComplaintController : ControllerBase
+    {
+        [HttpPost("create")]
+        public ActionResult<int> CreateComplaint(Complaint complaint)
+        {
+            return Ok(0);
+        }
+    }
+}
