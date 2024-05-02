a491aa3663801fa884a36eee29a11d526b314bb8
MiloszS <m.sobczyk.official@gmail.com>
2024-05-02 09:26:35 +0200

    Add test projects for each backend part and move existsing tests to correct projects (#46)

--- Backend/Test/ProviderServiceTests.cs
+++ Backend/Test/ProviderServiceTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Api.Services.Implementations;
-using Domain.Common;
+﻿using Domain.Common;
+using Logic.Services.Implementations;
 
 namespace Test
 {
