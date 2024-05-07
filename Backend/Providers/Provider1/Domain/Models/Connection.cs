128e8ff124a1bfa1cafa36de4041fbdac7493705
Milosz Sobczyk <misob@eg.dk>
2024-05-07 17:13:04 +0200

    Add connection

---
+++ Backend/Providers/Provider1/Domain/Models/Connection.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Models;
+
+public class Connection
+{
+    private static int _idCounter = 0;
+    public int ID { get; } = ++_idCounter;
+    public (int StationID, DateTime Time) StartStationTime { get; set; }
+    public (int StationID, DateTime Time) EndStationTime { get; set; }
+    public List<Seat>? Seats { get; set; }
+    public Connection((int StationID, DateTime Time) startStationTime, (int StationID, DateTime Time) endStationTime, List<Seat>? seats = null)
+    {
+        StartStationTime = startStationTime;
+        EndStationTime = endStationTime;
+        Seats = seats;
+    }
+}
