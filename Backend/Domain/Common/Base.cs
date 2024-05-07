8e75d1b3e41ac7d1a739e76ddadf5f9b16374c71
domser2002 <domser2002@o2.pl>
2024-05-08 01:25:10 +0200

    tmp commit

--- Backend/Domain/Common/Base.cs
+++ Backend/Domain/Common/Base.cs
@@ -1,9 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Common;
 
 public class Base
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int ID { get; set; }
 }
