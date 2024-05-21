1b8342cbb36d3712f2f4e131d59bff2196ea7862
domser2002 <domser2002@o2.pl>
2024-05-21 09:18:58 +0200

    fix null references

--- Backend/Logic/Services/Implementations/PaymentService.cs
+++ Backend/Logic/Services/Implementations/PaymentService.cs
@@ -8,6 +8,9 @@ public class PaymentService : IPaymentService
 {
     public bool ProceedPayment(Payment payment)
     {
+        if(payment is null) return false;
+        Thread.Sleep(1000);
+        if(payment.Code is null) return false;
         Thread.Sleep(1000);
         if(payment.Code.Length != 6 || !payment.Code.All(c => c >= '0' && c <= '9')) return false;
         Thread.Sleep(2000);
