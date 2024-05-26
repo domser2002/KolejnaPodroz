db098d1181c8c8c4f9f5e892a5857ee68b1723ca
domser2002 <domser2002@o2.pl>
2024-05-26 14:25:15 +0200

    reorganize connection model

---
+++ Backend/Domain/Common/StopDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Common
+{
+    public class StopDetails : Base
+    {
+        public DateTime? ArrivalTime { get; set; } // null for first station
+        public DateTime? DepartureTime { get; set; } // null for last station
+    }
+}
