038f7e53873a8bcee48bd22782af34e5fbc1dc35
domser2002 <domser2002@o2.pl>
2024-06-05 08:51:25 +0200

    added station service

---
+++ Backend/Logic/Services/Implementations/StationService.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Common;
+using Infrastructure.DataRepositories;
+using Infrastructure.Interfaces;
+using Logic.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Services.Implementations
+{
+    public class StationService(IDataRepository repository) : IStationService
+    {
+        private IDataRepository _repository = repository;
+        public Station? GetByID(int stationID)
+        {
+            return _repository.StationRepository.GetByID(stationID);
+        }
+    }
+}
