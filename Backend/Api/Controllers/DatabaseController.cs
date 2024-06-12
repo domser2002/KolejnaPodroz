ad395c5cc607664a598ca759516567b4c7a3ed41
Dominik Seredyn <86359641+domser2002@users.noreply.github.com>
2024-06-12 10:49:35 +0200

    implement error checking (#140)

--- Backend/Api/Controllers/DatabaseController.cs
+++ Backend/Api/Controllers/DatabaseController.cs
@@ -1,13 +1,15 @@
-﻿using Logic.Services.Interfaces;
+﻿using Infrastructure.Interfaces;
+using Logic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("Database")]
-public class DatabaseController(IDatabaseService databaseService) : ControllerBase
+public class DatabaseController(IDatabaseService databaseService, IDataRepository repository) : ControllerBase
 {
     private readonly IDatabaseService _databaseService = databaseService;
+    private readonly IDataRepository Repository = repository;
 
     [HttpGet("sqlQuery")]
     public ActionResult<List<Object[]>>QueryDatabase(string query)
@@ -21,5 +23,19 @@ public class DatabaseController(IDatabaseService databaseService) : ControllerBa
         {
             return StatusCode(500);
         }
+    }
+
+    [HttpGet("valid")]
+    public ActionResult<bool> CheckError()
+    {
+        try
+        {
+            Repository.UserRepository.GetAll();
+        }
+        catch(Exception)
+        {
+            return Ok(false);
+        }
+        return Ok(true);
     }
 }
